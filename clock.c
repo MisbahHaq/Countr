2c4ef6770de65b52e77d661a8bacb06f8308bc87
Misbah Haque <misbahmuneer2003@gmail.com>
2025-08-29 12:01:55 +0500

    Voila, Current Time Appears

---
+++ clock.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <time.h>
+
+int main()
+{
+    time_t raw_time;
+    struct tm *current_time;
+    char buffer[50];
+
+    time(&raw_time);
+    current_time = localtime(&raw_time);
+    strftime(buffer, sizeof(buffer), "%H:%M:%S", current_time);
+
+    printf("Current Time: %s", buffer);
+
+    return 0;
+}
